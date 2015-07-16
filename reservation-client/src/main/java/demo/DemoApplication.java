e0e0b194da95b7db93d52ff82aafd15cff0f8750
Josh Long <josh@joshlong.com>
2015-07-16 17:11:11 +0800

    improvements to the client thanks to the amazing https://github.com/olivergierke

--- reservation-client/src/main/java/demo/DemoApplication.java
+++ reservation-client/src/main/java/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package demo;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,20 +10,20 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.net.URI;
 
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
-@EnableZuulProxy
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableZuulProxy
 @SpringBootApplication
 public class DemoApplication {
 
@@ -40,20 +39,19 @@ public class DemoApplication {
     CommandLineRunner rt(RestTemplate rt) {
         return args -> {
 
-            List<HttpMessageConverter<?>> messageConverters = rt.getMessageConverters();
-            messageConverters.forEach(x -> System.out.println(x));
+            ParameterizedTypeReference<Resources<Reservation>> ptr =
+                    new ParameterizedTypeReference<Resources<Reservation>>() {
+                    };
 
-            ParameterizedTypeReference<Resources<Reservation>> ptr = new ParameterizedTypeReference<Resources<Reservation>>() {
-            };
             ResponseEntity<Resources<Reservation>> re = rt.exchange(
-                    "http://reservation-service/reservations",
-                    HttpMethod.GET,
-                    null,
-                    ptr);
-            System.out.println(re.getHeaders().getContentType().toString());
-            System.out.println(ToStringBuilder.reflectionToString(re.getBody()));
-            re.getBody().getContent().forEach(System.out::println);
+                    RequestEntity.get(URI.create("http://reservation-service/reservations"))
+                            .accept(MediaTypes.HAL_JSON)
+                            .build(),
+                    ptr
+            );
 
+            re.getBody().forEach(System.out::println);
+            re.getBody().getLinks().forEach(System.out::println);
         };
     }
 
@@ -61,68 +59,19 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
-/*
-
-@FeignClient("reservation-service")
-interface ReservationClient {
-
-    @RequestMapping(method = RequestMethod.GET, value = "/reservations")
-    Collection<Reservation> getReservations();
-}
-*/
 
 class Reservation {
-    private Long id;
     private String reservationName;
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Reservation{");
-        sb.append("id=").append(id);
-        sb.append(", reservationName='").append(reservationName).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
-
-    public Long getId() {
-        return id;
-    }
 
     public String getReservationName() {
         return reservationName;
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Reservation{");
+        sb.append("reservationName='").append(reservationName).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
-/*
-
-@Component
-class ReservationIntegration {
-
-    @Autowired
-    private ReservationClient reservationClient;
-
-    public Collection<String> getReservationNamesFallback() {
-        return Collections.emptyList();
-    }
-
-    @HystrixCommand (fallbackMethod = "getReservationNamesFallback")
-    public Collection<String> getReservationNames() {
-        return this.reservationClient.getReservations()
-                .stream()
-                .map(Reservation::getReservationName)
-                .collect(Collectors.toList());
-    }
-
-}
-
-@RestController
-class ReservationNameController {
-
-    @Autowired
-    private ReservationIntegration reservationIntegration;
-
-    @RequestMapping("/names")
-    Collection<String> reservationNames() {
-        return this.reservationIntegration.getReservationNames();
-    }
-
-}*/
